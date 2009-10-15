1872e7c8395455a94bf67f0d6fab0bdeb11f1770
Kyle Maxwell <kyle@kylemaxwell.com>
2009-10-15 11:21:46 -0700

    make post indexable

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,10 @@
 class Post < ActiveRecord::Base
-  Sunspot.setup(Post) do
-    text :title, :body
-    text :comments do
-      comments.map { |comment| comment.body }
-    end
-    integer :blog_id
-    integer :author_id
-    integer :category_ids, :multiple => true
-    time :published_at
+  searchable do
+    text :title, :content
+    integer :id
+    time :updated_at
     string :sort_title do
-      title.downcase.gsub(/^(an?|the)/, '')
+      title.downcase.sub(/^(an?|the) /, '')
     end
   end
 end
