128844d9033bd559d45d07458c22826c911dc738
Kyle Maxwell <kyle@kylemaxwell.com>
2009-10-16 14:11:42 -0700

    reorder gems

--- config/environment.rb
+++ config/environment.rb
@@ -23,9 +23,9 @@ Rails::Initializer.run do |config|
   config.gem "optiflag"   # sunspot dependency
   config.gem "escape"     # sunspot dependency
   config.gem "haml"     # sunspot dependency
-  config.gem "websolr-rails", :version => '>= 2', :source => "http://gemcutter.org"
   config.gem "outoftime-sunspot", :source => "http://gems.github.com", :lib => "sunspot"
   config.gem "outoftime-sunspot_rails", :source => "http://gems.github.com", :lib => "sunspot/rails"
+  config.gem "websolr-rails", :version => '>= 2', :source => "http://gemcutter.org"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
