8a0a2008b51e456777ce1d97a3372cc138127c1c
Kyle Maxwell <kyle@kylemaxwell.com>
2010-03-17 12:58:45 -0700

    now testing 1.0.3

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   
-  config.gem "websolr-sunspot_rails"
+  config.gem "websolr-sunspot_rails", :version => ">1"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
